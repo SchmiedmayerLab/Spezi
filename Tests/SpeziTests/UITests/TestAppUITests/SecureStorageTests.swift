f342b895b9c54b73e2423597253a8e58bcdebf92
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-10-27 09:11:24 -0700

    Introduce a UITests Project (#6)

---
+++ Tests/SpeziTests/UITests/TestAppUITests/SecureStorageTests.swift
@@ -0,0 +1,28 @@
+//
+// This source file is part of the CardinalKit open-source project
+//
+// SPDX-FileCopyrightText: 2022 CardinalKit and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+
+final class SecureStorageTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        continueAfterFailure = false
+    }
+    
+
+    func testSecureStorage() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.tabBars["Tab Bar"].buttons["SecureStorageTests"].tap()
+        
+        XCTAssertTrue(app.staticTexts["Passed"].waitForExistence(timeout: 2))
+    }
+}
