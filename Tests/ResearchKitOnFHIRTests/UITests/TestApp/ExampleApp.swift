c1b0ece8613d0eafd25109b447e07e491b729551
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-05-20 21:55:53 -0700

    Update to ResearchKit 3.0 (#81)
    # Update to ResearchKit 3.0
    
    ## :recycle: Current situation & Problem
    - ResearchKit 3.0 was released
    
    ## :gear: Release Notes 
    - Updates ResearchKitOnFHIR to ResearchKit 3.0
    - Move UI tests in the Tests folder to be consistent with all
    @StanfordSpezi & @StanfordBDHG packages.
    
    
    ### Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).
    
    ---------
    
    Co-authored-by: Vishnu Ravi <vishnur@stanford.edu>

---
+++ Tests/ResearchKitOnFHIRTests/UITests/TestApp/ExampleApp.swift
@@ -0,0 +1,22 @@
+//
+// This source file is part of the ResearchKitOnFHIR open source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import FHIRQuestionnaires
+import ResearchKitOnFHIR
+import SwiftUI
+
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            QuestionnaireListView()
+                .environment(QuestionnaireResponseStorage())
+        }
+    }
+}
