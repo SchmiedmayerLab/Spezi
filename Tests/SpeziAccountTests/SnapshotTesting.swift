86a77ea67aa111390f1655e4a841b84d3d37508f
Lukas Kollmer <hey@lukaskollmer.de>
2026-06-24 21:54:48 +0200

    fix SpeziAccount tests

--- Tests/SpeziAccountTests/SnapshotTesting.swift
+++ Tests/SpeziAccountTests/SnapshotTesting.swift
@@ -6,6 +6,8 @@
 // SPDX-License-Identifier: MIT
 //
 
+#if os(iOS)
+
 import SnapshotTesting
 @_spi(_Testing)
 @_spi(TestingSupport)
@@ -14,15 +16,9 @@ import SpeziTesting
 import SwiftUI
 import Testing
 
-#if os(iOS)
-let isRunningIOS = true
-#else
-let isRunningIOS = false
-#endif
 
 @Suite(
     "iOS Snapshot tests",
-    .enabled(if: isRunningIOS, "Requires iOS to run"),
     .disabled("""
         Disabled because the Text Fields don't render in the snapshots anymore,
         and we'd need a host application be present in order to be able to set
@@ -37,13 +33,10 @@ struct SnapshotTesting {
         let viewFalse = BoolDisplayView<MockBoolKey>(false)
         let viewTrueYes = BoolDisplayView<MockBoolKey>(label: .yesNo, true)
         let viewFalseNo = BoolDisplayView<MockBoolKey>(label: .yesNo, false)
-
-#if os(iOS)
         assertSnapshot(of: viewTrue, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-viewTrue")
         assertSnapshot(of: viewFalse, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-viewFalse")
         assertSnapshot(of: viewTrueYes, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-viewTrueYes")
         assertSnapshot(of: viewFalseNo, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-viewFalseNo")
-#endif
     }
     
     @MainActor
@@ -51,11 +44,8 @@ struct SnapshotTesting {
     func testIntegerDisplayView() {
         let integer = FixedWidthIntegerDisplayView<MockNumericKey>(34)
         let integerWithUnit = FixedWidthIntegerDisplayView<MockNumericKey>(34, unit: Text(verbatim: " cm"))
-
-#if os(iOS)
         assertSnapshot(of: integer, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-integer")
         assertSnapshot(of: integerWithUnit, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-integerWithUnit")
-#endif
     }
     
     @MainActor
@@ -63,30 +53,22 @@ struct SnapshotTesting {
     func testFloatingPointDisplayView() {
         let float = FloatingPointDisplayView<MockDoubleKey>(23.56)
         let floatWithUnit = FloatingPointDisplayView<MockDoubleKey>(223.56, unit: Text(verbatim: " cm"))
-
-#if os(iOS)
         assertSnapshot(of: float, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-float")
         assertSnapshot(of: floatWithUnit, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-floatWithUnit")
-#endif
     }
 
     @MainActor
     @Test
     func testStringDisplayView() {
         let view = StringDisplayView(\.userId, "Hello World")
-
-#if os(iOS)
         assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone")
-#endif
     }
 
     @MainActor
     @Test
     func testLocalizedStringDisplayView() {
         let view = LocalizableStringDisplayView(\.genderIdentity, .male)
-#if os(iOS)
         assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone")
-#endif
     }
 
     
@@ -135,23 +117,20 @@ struct SnapshotTesting {
         let view4Signup = view4.preferredAccountSetupStyle(.signup)
         let view5Signup = view5.preferredAccountSetupStyle(.signup)
 
-#if os(iOS)
-        withSnapshotTesting(diffTool: .ksdiff) {
-            assertSnapshot(of: view0, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view0")
-            assertSnapshot(of: view1, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view1")
-            assertSnapshot(of: view2, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view2")
-            assertSnapshot(of: view3, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view3")
-            assertSnapshot(of: view4, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view4")
-            assertSnapshot(of: view5, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view5")
+        assertSnapshot(of: view0, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view0")
+        assertSnapshot(of: view1, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view1")
+        assertSnapshot(of: view2, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view2")
+        assertSnapshot(of: view3, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view3")
+        assertSnapshot(of: view4, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view4")
+        assertSnapshot(of: view5, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view5")
 
-            assertSnapshot(of: view0Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view0-signup")
-            assertSnapshot(of: view1Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view1-signup")
-            assertSnapshot(of: view2Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view2-signup")
-            assertSnapshot(of: view3Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view3-signup")
-            assertSnapshot(of: view4Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view4-signup")
-            assertSnapshot(of: view5Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view5-signup")
+        assertSnapshot(of: view0Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view0-signup")
+        assertSnapshot(of: view1Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view1-signup")
+        assertSnapshot(of: view2Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view2-signup")
+        assertSnapshot(of: view3Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view3-signup")
+        assertSnapshot(of: view4Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view4-signup")
+        assertSnapshot(of: view5Signup, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone-view5-signup")
         }
-#endif
     }
     
     @MainActor
@@ -164,8 +143,8 @@ struct SnapshotTesting {
         let view = AccountHeader(caption: "Custom Caption")
             .environment(configuration.account)
 
-#if os(iOS)
         assertSnapshot(of: view, as: .image(layout: .device(config: .iPhone13Pro)), named: "iphone")
-#endif
     }
 }
+
+#endif
